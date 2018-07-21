aa594739563a047234e5567b87eeafc7306d6bdf
league <league@imac-7.attlocal.net>
2018-07-21 15:55:09 -0700

    gmjfkf

---
+++ src/HarryRunner.java
@@ -0,0 +1,12 @@
+
+public class HarryRunner {
+	public static void main(String[] args) {
+		
+		String Cloak = "...invisible";
+		
+		String Spell = "Stupefy!";
+		
+		
+	}
+
+}
--- src/RaceCarRunner.java
+++ src/RaceCarRunner.java
@@ -5,14 +5,24 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+		String Ford = "Ford";
+		int position =5;
+		RaceCar SNES= new RaceCar(Ford, position);
 		// 2. Print the RaceCar's position in the race
-
+		System.out.println(position);
 		// 3. Crash the RaceCar
-		
+		SNES.crash();
 		// 4. If the car is damaged. Bring it in for a pit stop.
+		if (SNES.isDamaged()) {
+		SNES.pit();
+		} else {
+		SNES.getPositionInRace();		
 
+		}
 		// 5. Help the car move into first place.
-
+		for (int i = 0; i < 8; i++) {
+			SNES.overtake();
+			
+		}
 	}
 }
