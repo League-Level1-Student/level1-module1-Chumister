1f45b7233656d08de6eccaff877ff647d9127c05
Kaiya <league@imac-3.attlocal.net>
2018-07-28 15:56:29 -0700

    vc c

---
+++ src/CatRunner.java
@@ -0,0 +1,10 @@
+
+public class CatRunner {
+public static void main(String[] args) {
+	
+Cat Garfield = new Cat(null);
+Garfield.meow();
+Garfield.printName();
+Garfield.kill();
+}
+}
--- src/HarryRunner.java
+++ src/HarryRunner.java
@@ -2,11 +2,9 @@
 public class HarryRunner {
 	public static void main(String[] args) {
 		
-		String Cloak = "...invisible";
-		
-		String Spell = "Stupefy!";
-		
-		
+ HarryPotter Harry= new HarryPotter();		
+ Harry.spyOnSnape();
+ Harry.castSpell("Sectumsempra");
 	}
 
 }
